ba5268232d934626ca2e2ea6c2aab91e496d509e
Thiago Souza Cardoso <thiagocardosok@gmail.com>
2021-11-19 10:22:32 -0300

    init

--- consumer/src/main/java/com/example/consumer/configurations/KafkaConfig.java
+++ consumer/src/main/java/com/example/consumer/configurations/KafkaConfig.java
@@ -1,15 +1,8 @@
 package com.example.consumer.configurations;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.converter.StringJsonMessageConverter;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 @EnableKafka
--- consumer/src/main/java/com/example/consumer/entrypoints/kafka/ReplyingKafkaConsumer.java
+++ consumer/src/main/java/com/example/consumer/entrypoints/kafka/ReplyingKafkaConsumer.java
@@ -2,12 +2,12 @@ package com.example.consumer.entrypoints.kafka;
 
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.confluent.developer.StudentMessageInput;
+import io.confluent.developer.StudentMessageOutput;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
-import io.confluent.developer.StudentInput;
-import io.confluent.developer.StudentOutput;
 
 import java.util.UUID;
 
@@ -17,10 +17,10 @@ public class ReplyingKafkaConsumer {
 	 
 	 @KafkaListener(topics = "${kafka.topic.request-topic}")
 	 @SendTo
-	  public StudentOutput listen(ConsumerRecord<String, StudentInput> payload) throws InterruptedException, JsonProcessingException {
+	  public StudentMessageOutput listen(ConsumerRecord<String, StudentMessageInput> payload) throws InterruptedException, JsonProcessingException {
 		 System.out.println(payload.value());
 
-		 StudentOutput studentOutput = StudentOutput.newBuilder().setUuid(UUID.randomUUID().toString()).build();
+		 StudentMessageOutput studentOutput = StudentMessageOutput.newBuilder().setUuid(UUID.randomUUID().toString()).build();
 
 		 return studentOutput;
 	  }
